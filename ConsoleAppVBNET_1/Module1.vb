754f70d6e87a9a0ae911f40d6f9df79806933e86
witkowt2 <tomasz.witkowski.tw2@contractors.roche.com>
2019-05-31 15:39:27 +0200

    Add function Signature

--- ConsoleAppVBNET_1/Module1.vb
+++ ConsoleAppVBNET_1/Module1.vb
@@ -1,10 +1,18 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim name As String = "Tomasz Witkowski"
+
         Console.WriteLine("My name is Thomas")
         Console.WriteLine("This is branch 1")
+        Console.WriteLine(Signature(name))
         Console.ReadKey()
 
     End Sub
+
+    Function Signature(ByVal name) As String
+        Signature = "My name is:" & name
+    End Function
+
 
 End Module
