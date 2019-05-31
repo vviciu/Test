982cd1de665f5909af0752bc8763ec4804ecebd5
witkowt2 <tomasz.witkowski.tw2@contractors.roche.com>
2019-05-31 15:42:27 +0200

    Feature add write line to consol.

--- ConsoleAppVBNET_1/Module1.vb
+++ ConsoleAppVBNET_1/Module1.vb
@@ -3,6 +3,7 @@
     Sub Main()
         Console.WriteLine("My name is Thomas")
         Console.WriteLine("This is branch 1")
+        Console.WriteLine("This is line added before marge from future1")
         Console.ReadKey()
 
     End Sub
